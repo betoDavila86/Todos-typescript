c805899f64dbd58d9d456dcc394f0c777caa8814
Alberto Davila <alberto.davila.gomez@gmail.com>
2021-08-12 10:44:40 +0200

    base project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Todo app
+Just a simple todo app to practice Typescript and some extra features provided by React.
+
+### Tech stack
+- React (using Context API)
+- Typescript
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Todos from "./Components/Todos/Todos";
+
+function App() {
+  return (
+    <div>
+      <Todos todos={["Holaaa", "Adiosss", "Amiggo"]}/>
+    </div>
+  );
+}
+
+export default App;
